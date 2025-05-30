fc4d67dac41a3e35f8559bda975495625fcc5bd6
fujiij <32182215+fujiij@users.noreply.github.com>
2025-05-30 12:47:36 +0900

    Add files

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "jp.kthrlab"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"), "exclude" to listOf<String>())))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
